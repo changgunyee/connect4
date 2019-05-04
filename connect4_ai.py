66545c964ba0112b431028eb3e680096ab2c9592
changgunyee <changgunyee@github.com>
2019-05-04 23:33:54 +0900

    detect다시 추가

--- connect4_ai.py
+++ connect4_ai.py
@@ -24,7 +24,10 @@ def win_recursive(board, alpha, beta,level=0):
         for col_index in range(0, COLUMN_SIZE):
             if board[5, col_index] == -1 :
                 child_board = copyChildBoard(board, col_index, level)
-                array[col_index] = win_recursive(child_board, alpha, beta, level + 1)
+                if type(child_board) == bool and child_board == False:
+                    array[col_index] = MAX_COUNT
+                else:
+                    array[col_index] = win_recursive(child_board, alpha, beta, level + 1)
                 max_wincount = max(array[col_index], max_wincount)
                 alpha = max(alpha, max_wincount)
                 if beta <= alpha:
@@ -40,7 +43,10 @@ def win_recursive(board, alpha, beta,level=0):
         for col_index in range(0, COLUMN_SIZE):
             if board[5, col_index] == -1:
                 child_board=copyChildBoard(board, col_index, level)
-                array[col_index] = win_recursive(child_board, alpha, beta, level + 1)
+                if type(child_board) == bool and child_board == False:
+                    array[col_index] = -MAX_COUNT
+                else :
+                    array[col_index] = win_recursive(child_board, alpha, beta, level + 1)
                 min_wincount = min(array[col_index], min_wincount)
                 beta = min(beta, min_wincount)
                 if beta <= alpha:
@@ -54,19 +60,47 @@ def win_recursive(board, alpha, beta,level=0):
 def copyChildBoard(parentBoard, col_index, level): #col_index 의 height만 알아내면 됩니다
     childBoard = np.copy(parentBoard)
     column_height = 0
+    #if parentBoard[ROW_SIZE-1,col_index] == -1 :
+        #column_height = ROW_SIZE
+    #else :
+        #column_height = np.argmin(parentBoard[:,col_index])
     for i in range(ROW_SIZE):
         if (parentBoard[i, col_index] == -1):
             column_height = i
             break
 
-    if (level % 2 == 0):  # 짝수인 경우 AI 차례!
-        childBoard[column_height, col_index] = 1
 
-    else:  # 홀수인 경우 상대방 차례
-        childBoard[column_height, col_index] = 0
+    # if (level % 2 == 0):  # 짝수인 경우 AI 차례!
+    #     arr=[1,1,1,1]
+    #     childBoard[column_height, col_index] = 1
+    #
+    # else:  # 홀수인 경우 상대방 차례
+    #     arr[0,0,0,0]
+    #     childBoard[column_height, col_index] = 0
+
+    level_modulo_2=(level+1)%2
+    childBoard[column_height, col_index] = level_modulo_2
+    arr=[level_modulo_2 ,level_modulo_2 ,level_modulo_2 ,level_modulo_2 ]
+
+    if column_height > 3 and childBoard[column_height - 3:column_height + 1, col_index].tolist() == arr: #linetype |
+        return False
+
+    for col in range(COLUMN_SIZE - 3): #linetype -
+        if childBoard[column_height, col:col + 4].tolist() == arr:
+            return False
+
+    for column in range(COLUMN_SIZE - 3): #linetype /
+        for row in range(ROW_SIZE - 3):
+            if childBoard[row:row+4,column:column+4].diagonal().tolist()==arr:
+                return False
+
+    for column in range(COLUMN_SIZE - 3): #linetype \
+        for row in range(3, ROW_SIZE):
+            if childBoard[row][column] == level_modulo_2 and childBoard[row - 1][column + 1] == level_modulo_2 and childBoard[row - 2][
+                column + 2] == level_modulo_2 and childBoard[row - 3][column + 3] == level_modulo_2:
+                return False
 
     return childBoard
-
 
 def evaluate(board,player):
     enemy= 0 if player == 1 else 1
@@ -148,10 +182,13 @@ def getPoint(row,player,start_idx,end_idx):
     if chessman_num==3: #Feature 2
         if start_idx-1>=0 and row[start_idx-1]==-1:#Feature2-2
             if end_idx+1<7 and row[end_idx+1]==-1:#Feature2-1
-                point+=MAX_COUNT
+                return MAX_COUNT
             point+=90000
         else:
             if end_idx + 1 < 7 and row[end_idx + 1] == -1:#Feature2-2
                 point += 90000
+
+    if chessman_num==4:
+        return MAX_COUNT
 
     return point
