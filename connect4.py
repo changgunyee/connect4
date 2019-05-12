6ffd6bae22988a6c7bcceef79304dffd9f2ecb80
changgunyee <changgunyee@github.com>
2019-05-12 21:15:37 +0900

    complete version

--- connect4.py
+++ connect4.py
@@ -4,7 +4,7 @@ ROW_SIZE = 6
 COLUMN_SIZE = 7
 board = np.full((ROW_SIZE, COLUMN_SIZE), -1)
 column_height = [0 for num in range(COLUMN_SIZE)]
-
+DRAW = 0
 menu_str = "1 ~ 7번 중 컬럼 선택하세요."
 
 """AI는 1번, 사용자는 0번입니다."""
@@ -25,13 +25,20 @@ def print_board():
 
 def choose(column, user=1):  # AI가 두는것이 1
     column -= 1
-    if column_height[column] >= ROW_SIZE or column < 0 or column >= COLUMN_SIZE:
-        return False
+    if column_height[column] >= ROW_SIZE:
+        print("해당 column은 가득찼습니다. 다른 column을 선택해주세요")
+        return 0, False
+    if (column == -1 and user == 1) or sum(column_height) == 36:
+        print("무승부입니다.")
+        return 1, True
+    if column < 0 or column >= COLUMN_SIZE:
+        print("해당 column에는 둘 수 없습니다. 다른 column을 선택해주세요")
+        return 0, False
 
     board[column_height[column]][column] = user
     column_height[column] += 1
     print_board()
-    return True
+    return 0, True
 
 
 def is_game_over(user):
@@ -62,6 +69,7 @@ def is_game_over(user):
 
 first_person = int(input("선공:1, 후공:2  선택하세요. :"))
 global person_choice
+person_choice = -1
 if first_person == 1:
     while True:
         person_choice = int(input(menu_str))
@@ -70,26 +78,31 @@ if first_person == 1:
                 break
             print("선공은 4번 컬럼에 놓을수 없습니다")
             person_choice = int(input(menu_str))
-        if choose(person_choice, 0):  # 사람이 두는것이 0
+        DRAW, ACCURATE_POS = choose(person_choice, 0)
+        if ACCURATE_POS:  # 사람이 두는것이 0
             break
 
 while True:
     print("AI가 밑에 처럼 놓았습니다.")
     while True:
         choice_ai = ai(np.copy(board), person_choice - 1)
-        print("AI_choice : ", choice_ai)
-        if choose(choice_ai + 1):  # AI가 두는것이 1
+        print("AI_choice : ", choice_ai + 1)
+        DRAW, ACCURATE_POS = choose(choice_ai + 1)
+        if ACCURATE_POS:  # AI가 두는것이 1
             break
-
+    if DRAW == 1:
+        break
     if is_game_over(1):
         print("AI가 승리하였습니다.")
         break
 
     while True:
         person_choice = int(input(menu_str))
-        if choose(person_choice, 0):
+        DRAW, ACCURATE_POS = choose(person_choice, 0)
+        if ACCURATE_POS:
             break
-
+    if DRAW == 1:
+        break
     if is_game_over(0):
         print("당신이 승리하였습니다.")
         break
--- connect4_ai.py
+++ connect4_ai.py
@@ -14,14 +14,14 @@ STORE_NEXT_CHOICE = np.full(COLUMN_SIZE * COLUMN_SIZE, -MAX_COUNT)
 def ai(board, person_choice):
     global NOT_FIRST
     global STORE_NEXT_CHOICE
-    if np.sum(board[0, :]) == -7:  # AI 가 선공일 때 4th column에 두면 안됩니다
-        return 2  # 4th가 아닌 3th column에 두도록
+    if np.sum(board[0, :]) == -7:   # AI 가 선공일 때 4th column에 두면 안됩니다
+        return 2                    # 4th가 아닌 3th column에 두도록
     else:
         if (NOT_FIRST == 0):
             SEARCH_COLUMN = [3, 2, 4, 1, 5, 0, 6]
         else:
             SEARCH_COLUMN = np.argsort(-(STORE_NEXT_CHOICE[person_choice * 7: person_choice * 7 + 7]))
-        print("person_choice : ", person_choice)
+        print("person_choice : ", person_choice+1)
         print("STORE_NEXT_CHOICE에 저장된 값 : ", STORE_NEXT_CHOICE[person_choice * 7: person_choice * 7 + 7])
         print("COLUMN 검색하는 순서 : ", SEARCH_COLUMN)
         global DEPTH
@@ -38,7 +38,7 @@ def ai(board, person_choice):
             if left_time < 6 * runing_time or DEPTH > 14:
                 break
             DEPTH += 1
-        print("최종 Depth:%d" % DEPTH)
+            print(DEPTH,"의 깊이 만큼 search결과 저희 heuristic값은 위의 배열과 같습니다.",bestCol+1,"의 열에 수를 둘 경우 heuristic값이 MAX입니다. ")
         print("--- %s seconds ---" % (time.time() - first_start_time))
 
         return bestCol
@@ -66,13 +66,13 @@ def win_recursive(board, alpha, beta, SEARCH_COLUMN, level=0, parent_col=-1):
             STORE_LV3_POINT[parent_col * 7: parent_col * 7 + 7] = array[:]
 
         if level == 0:
-            max_point = -MAX_COUNT
+            max_point = -MAX_COUNT-1
             max_point_index = -1
             for i in range(0, COLUMN_SIZE):
-                if board[5, col_index] != -1:
+                if board[5, i] != -1:
                     array[i] = -MAX_COUNT - 1
             print(array)
-            for i in SEARCH_COLUMN:
+            for i in [3, 2, 4, 1, 5, 0, 6]:
                 if max_point < array[i]:
                     max_point = array[i]
                     max_point_index = i
@@ -96,11 +96,8 @@ def win_recursive(board, alpha, beta, SEARCH_COLUMN, level=0, parent_col=-1):
                     break
         if level == 1:
             STORE_LV2_POINT[parent_col * 49:parent_col * 49 + 49] = STORE_LV3_POINT[:]
-        if level == 0:
-            print(array)
-            return np.argmax(array)
-        else:
-            return min_wincount
+
+        return min_wincount
 
 
 def copyChildBoard(parentBoard, col_index, level):  # col_index 의 height만 알아내면 됩니다
@@ -141,106 +138,307 @@ def copyChildBoard(parentBoard, col_index, level):  # col_index 의 height만 �
 def evaluate(board, player):
     enemy = 0 if player == 1 else 1
     total_point = 0
-    for row in range(len(board)):  # linetype -
-        total_point += pointByFeature(board[row], player)
+    check_point = -1
+    total_empty_count=0
+    column_empty_count=[ 0 for column in range(COLUMN_SIZE)]
+    for column in range(COLUMN_SIZE):
+        for row in range(ROW_SIZE):
+            if board[row][column]==-1:
+                column_empty_count[column]=6-row
+                total_empty_count+=6-row
+                break
+    for column in range(COLUMN_SIZE):
+        column_empty_count[column]=total_empty_count-column_empty_count[column]
 
-    for column in range(COLUMN_SIZE):  # linetype |
-        row = np.append(board[:, column], [enemy])
-        total_point += pointByFeature(row, player, column)
+    for row in range(len(board)):                                                #linetype -
+        temp, psbType = pointByFeature(board[row], player)
+        check_point = checkPossible(psbType, board[row], player)
+        if(psbType == -1 or check_point == -1):
+            total_point += temp
+        else:
+            height = 0
+            for i in range(0, row):
+                if(board[i][check_point]==-1):               # = board[i][check_point] == -1
+                    height += 1
+            if((height + column_empty_count[check_point]) % 2 == 1):                #남은 수 = 해당 컬럼 제외 남은 수 + height, 가 짝수면 내가 못 놓음. 너나너나너나 로 끝나서 상대가 너로 놓게됨
+                total_point += temp
+            else:
+                total_point += temp / 2
 
-    total_point += pointByFeature(np.append(board[2:6, 0:4].diagonal(), [enemy, enemy, enemy]), player)  # linetype /
-    total_point += pointByFeature(np.append(board[1:6, 0:5].diagonal(), [enemy, enemy]), player)
-    total_point += pointByFeature(np.append(board[0:6, 0:6].diagonal(), [enemy]), player)
-    total_point += pointByFeature(np.append([enemy], board[0:6, 1:7].diagonal()), player)
-    total_point += pointByFeature(np.append([enemy, enemy], board[0:5, 2:7].diagonal()), player)
-    total_point += pointByFeature(np.append([enemy, enemy, enemy], board[0:4, 3:7].diagonal()), player)
+    for column in range(COLUMN_SIZE):                                           #linetype |
+        row=np.append(board[:,column],[enemy])
+        temp, psbType = pointByFeature(row,player,column)
+        total_point += temp
 
-    total_point += pointByFeature(np.append([enemy, enemy, enemy], np.fliplr(board[0:4, 0:4]).diagonal()),
-                                  player)  # linetype \
-    total_point += pointByFeature(np.append([enemy, enemy], np.fliplr(board[0:5, 0:5]).diagonal()), player)
-    total_point += pointByFeature(np.append([enemy], np.fliplr(board[0:6, 0:6]).diagonal()), player)
-    total_point += pointByFeature(np.append(np.fliplr(board[0:6, 1:7]).diagonal(), [enemy]), player)
-    total_point += pointByFeature(np.append(np.fliplr(board[1:6, 2:7]).diagonal(), [enemy, enemy]), player)
-    total_point += pointByFeature(np.append(np.fliplr(board[2:6, 3:7]).diagonal(), [enemy, enemy, enemy]), player)
+    temp, psbType = pointByFeature(np.append(board[2:6,0:4].diagonal(), [enemy, enemy, enemy]), player) #linetype /
+    check_point = checkPossible(psbType, np.append(board[2:6, 0:4].diagonal(), [enemy, enemy, enemy]), player)
+    if(psbType == -1 or check_point == -1):
+        total_point+= temp
+    else:
+        height = 0
+        for i in range(0, check_point+2):
+            if(board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append(board[1:6,0:5].diagonal(),[enemy,enemy]),player)
+    check_point = checkPossible(psbType, np.append(board[1:6, 0:5].diagonal(), [enemy, enemy]), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point + 1):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height +column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append(board[0:6,0:6].diagonal(),[enemy]),player)
+    check_point = checkPossible(psbType, np.append(board[0:6, 0:6].diagonal(), [enemy]), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append([enemy],board[0:6,1:7].diagonal()),player)
+    check_point = checkPossible(psbType, np.append([enemy],board[0:6, 1:7].diagonal()), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point - 1):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append([enemy,enemy],board[0:5,2:7].diagonal()), player)
+    check_point = checkPossible(psbType, np.append([enemy,enemy],board[0:5, 2:7].diagonal()), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point -2 ):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append([enemy, enemy, enemy],board[0:4,3:7].diagonal()),player)
+    check_point = checkPossible(psbType, np.append([enemy, enemy, enemy],board[0:4, 3:7].diagonal()), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point - 3):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append([enemy,enemy,enemy],np.fliplr(board[0:4,0:4]).diagonal()),player) #linetype \
+    check_point = checkPossible(psbType, np.append([enemy, enemy, enemy],np.fliplr(board[0:4, 0:4]).diagonal()), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point - 3):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[3-check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append([enemy,enemy],np.fliplr(board[0:5,0:5]).diagonal()),player)
+    check_point = checkPossible(psbType, np.append([enemy,enemy],np.fliplr(board[0:5, 0:5]).diagonal()), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point - 2):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[4-check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append([enemy],np.fliplr(board[0:6, 0:6]).diagonal()),player)
+    check_point = checkPossible(psbType, np.append([enemy],np.fliplr(board[0:6, 0:6]).diagonal()), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point - 1):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[5-check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append(np.fliplr(board[0:6, 1:7]).diagonal(),[enemy]),player)
+    check_point = checkPossible(psbType, np.append(np.fliplr(board[0:6, 1:7]).diagonal(),[enemy]), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[6-check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append(np.fliplr(board[1:6,2:7]).diagonal(),[enemy,enemy]),player)
+    check_point = checkPossible(psbType, np.append(np.fliplr(board[1:6, 2:7]).diagonal(), [enemy, enemy]), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point + 1):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[6 - check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
+    temp, psbType = pointByFeature(np.append(np.fliplr(board[2:6,3:7]).diagonal(),[enemy,enemy,enemy]),player)
+    check_point = checkPossible(psbType, np.append(np.fliplr(board[2:6, 3:7]).diagonal(), [enemy, enemy, enemy]), player)
+    if (psbType == -1 or check_point == -1):
+        total_point += temp
+    else:
+        height = 0
+        for i in range(0, check_point + 2):
+            if (board[i][check_point] == -1):
+                height += 1
+        if((height + column_empty_count[check_point]) % 2 == 1):
+            total_point += temp
+        else:
+            total_point += temp / 2
+
     return total_point
-
 
 def pointByFeature(row, player, column=-1):
     start_idx = -1
     sum_point = 0
+    check_psb = -1                                      #getPoint 참조
+
     for idx, val in enumerate(row):
         if val == player and start_idx == -1:
             start_idx = idx
 
         if val != player and start_idx != -1:
-            sum_point += getPoint(row, player, start_idx, idx - 1, column)
+            temp1, temp2 = getPoint(row, player,start_idx, idx - 1, column)
+            sum_point += temp1
+            check_psb = temp2
             start_idx = -1
-    return sum_point
 
+    return sum_point, check_psb
 
-def getPoint(row, player, start_idx, end_idx, column):
-    chessman_num = end_idx - start_idx + 1
-    point = 0
+def getPoint(row,player,start_idx,end_idx, column):
+    chessman_num=end_idx-start_idx+1
+    point=0
+    check_possible = -1                        #기본 값 : -1, f 2-2 : 1, f 2-3 : 2
     enemy = 0 if player == 1 else 1
     front_is_empty = start_idx - 1 >= 0 and row[start_idx - 1] == -1
     back_is_empty = end_idx + 1 < 7 and row[end_idx + 1] == -1
 
-    if chessman_num == 1:  # Feature4
+    if chessman_num==1:  #Feature4
         if column == -1:
             column = start_idx
-        if front_is_empty or back_is_empty:
-            if column == 0 or column == 6:
-                point += 4
-            elif column == 1 or column == 5:
-                point += 7
-            elif column == 2 or column == 4:
-                point += 12
-            else:
-                point += 20
+        if column == 0 or column == 6:
+            point += 4
+        elif column == 1 or column == 5:
+            point += 7
+        elif column == 2 or column == 4:
+            point += 12
+        else:
+            point += 20
 
-    elif chessman_num == 2:
+    elif chessman_num==2:
         if back_is_empty and (front_is_empty == False):  # Feature 3
             temp = end_idx + 2
             if temp < 7 and row[temp] == player:  # Feature2-3
                 point += 90000
+                check_possible = 2
             else:
                 while temp < 7:
                     if row[temp] == enemy:
                         break
-                    point += 1000
+                    point += 500
                     temp += 1
 
-        if front_is_empty and (back_is_empty == False):  # Feature 3
+        elif front_is_empty and (back_is_empty == False):  # Feature 3
             temp = start_idx - 2
             if temp >= 0 and row[temp] == player:  # Feature2-3
                 point += 90000  # 앞쪽으로만 2-3케이스를 추가하기위해
+                check_possible = 2
             else:
                 while temp >= 0:
                     if row[temp] == enemy:
                         break
-                    point += 1000
+                    point += 500
                     temp -= 1
 
-        if front_is_empty and back_is_empty:  # Feature2-4
+        elif front_is_empty and back_is_empty:  # Feature2-4
             temp1 = end_idx + 2
             temp2 = start_idx - 2
             if temp1 < 7 and row[temp1] == player and temp2 >= 0 and row[temp2] == player:
-                point += 180000
-            point += 5000
-
-
-    elif chessman_num == 3:  # Feature 2
-        if front_is_empty:  # Feature2-2
-            if back_is_empty:  # Feature2-1
-                point += 180000
+                point += 100000
+                check_possible = 2
             else:
-                point += 90000
+                point += 2500
+
+    elif chessman_num==3: #Feature 2
+        if front_is_empty: #Feature2-2
+            if back_is_empty: #Feature2-1
+                return 100000, 1
+            point+=90000
+            check_possible = 1
         else:
-            if front_is_empty:  # Feature2-2
+            if back_is_empty:#Feature2-2
                 point += 90000
+                check_possible = 1
 
-    elif chessman_num == 4:
-        # print(row,"ERROR 났어용~~~~~~~~~~~~~~~~~~~")
-        return MAX_COUNT
+    return point, check_possible
 
-    return point
+def checkPossible(psb, list, player):
+    idx = -1
+    if(psb == -1):
+        return idx
+    elif(psb == 1):
+        for idx in range(0, 4):
+            if(list[idx] == -1 and list[idx+1] == player and list[idx+2] == player and list[idx+3] == player):
+                return idx
+        for idx in range(3, 7):
+            if(list[idx] == -1 and list[idx-1] == player and list[idx-2] == player and list[idx-3] == player):
+                return idx
+    elif(psb == 2):
+        for idx in range(1, 5):
+            if(list[idx-1] == player and list[idx] == -1 and list[idx+1] == player and list[idx+2] == player):
+                return idx
+        for idx in range(2, 6):
+            if(list[idx-2] == player and list[idx - 1] == player and list[idx] == -1 and list[idx+1] == player):
+                return idx
